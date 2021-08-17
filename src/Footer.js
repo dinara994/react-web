30b8041e69a697924cf7d19a69ab50de5ec41174
dinara994 <di_94@mail.ru>
2021-08-17 19:11:46 +0600

    initial

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,16 @@
-import React from "react"
+import React from "react";
 import './Footer.css'
-
 const Footer = () => {
-    return (
+
+return (
         <footer className='footer'>
             <p>Лучший магазин (с) 2019 Все права защищены. Интернет-магазин оборудования для салонов красоты</p>
             <p>Политика конфиденциальности</p>
         </footer>
     )
 }
+
 export default Footer
-
-
 
 
 
